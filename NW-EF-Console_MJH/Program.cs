aa3ce57f21158863eb24d1a4fe873163a7be8364
Mary Hughes <mhughes21@my.wctc.edu>
2019-04-17 08:22:55 -0500

    Add project files.

---
+++ NW-EF-Console_MJH/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NW_EF_Console_MJH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
